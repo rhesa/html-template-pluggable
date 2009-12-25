87a87e1129b27f817dbf2bfabe3a33082b3f499c
Rhesa Rozendaal <rhesa@cpan.org>
2009-12-26 00:15:07 +0100

    fixed RT #49123 (SILASMONK)

--- lib/HTML/Template/Plugin/Dot.pm
+++ lib/HTML/Template/Plugin/Dot.pm
@@ -180,7 +180,7 @@ THE_REST:
             //xi ) {
         my ($id, $data) = ($1, $2);
         if (blessed($ref)) {
-            if ($ref->can($id)) {
+            if ($ref->can($id) or $ref->can('AUTOLOAD')) {
                 my @args = ();
                 if ($data) {
                     $data =~ s/^\(// and $data =~ s/\)$//;
